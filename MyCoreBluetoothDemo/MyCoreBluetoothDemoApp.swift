53541db51b18f564f973195bdf10145e2d33922a
BMR11 <brewmerang@gmail.com>
2025-08-01 22:40:39 -0700

    Initial Commit

---
+++ MyCoreBluetoothDemo/MyCoreBluetoothDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreBluetoothDemoApp.swift
+//  MyCoreBluetoothDemo
+//
+//  Created by Shiv on 2025-08-01.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreBluetoothDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
